f4bb9d83368d82a424f26b4b7d6a546bd4a47a38
Joseph Herrera <josephherreradev@outlook.com>
2021-12-18 14:38:48 -0600

    Final commit to open an empty window

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import pygame
 pygame.init()
 
 #Create the screen
+screen = pygame.display.set_mode((800,600))
 
+#While cycle to keep the window open
+running = True
+while running:
+    #Checks for all events running
+    for event in pygame.event.get():
+        #Checks for the event QUIT to close the window
+        if event.type == pygame.QUIT:
+            running = False
